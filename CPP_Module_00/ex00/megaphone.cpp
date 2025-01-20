27f8fc085ba3b8022627695eca3f5ba1795413ba
doyoungkim-code <ehehkwat1@naver.com>
2025-01-20 17:11:24 +0900

    250120

--- CPP_Module_00/ex00/megaphone.cpp
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <cctype>
 
-int main()
+int main(int argc, char **argv)
 {
-    std::cout << "megaphone" << std::endl;
+    int i, j;
+
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    j = 1;
+    while (argv[j])
+    {
+        if (j >= 2)
+            std::cout << " ";
+        i = 0;
+        while (argv[j][i])
+        {
+            if (islower(argv[j][i]))
+                std::cout << (char)toupper(argv[j][i]);
+            else
+                std::cout << argv[1][i];
+            i ++;
+        }
+        j ++;
+    }
+    std::cout << std::endl;
     return (0);
 }
